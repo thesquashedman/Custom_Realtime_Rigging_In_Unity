5d64d1d32c43d5f34f8689d38a4ba5b68caab038
Pavel Peev <76268971+thesquashedman@users.noreply.github.com>
2022-11-21 21:54:52 -0800

    Vertices now correctly rotate around the bones

--- Assets/Source/Model/SceneNode.cs
+++ Assets/Source/Model/SceneNode.cs
@@ -41,11 +41,32 @@ public class SceneNode : MonoBehaviour {
     {
         Matrix4x4 orgT = Matrix4x4.Translate(NodeOrigin);
         Matrix4x4 trs = Matrix4x4.TRS(transform.localPosition, transform.localRotation, transform.localScale);
-        Matrix4x4 trsOrigin = Matrix4x4.TRS(transform.localPosition - oPosition, transform.localRotation * Quaternion.Inverse(oRotation), transform.localScale - oScale + Vector3.one);
+        //Matrix4x4 trsOrigin = Matrix4x4.TRS(transform.localPosition - oPosition, transform.localRotation * Quaternion.Inverse(oRotation), transform.localScale - oScale + Vector3.one);
         
+
+        Matrix4x4 T = Matrix4x4.identity;
+        Matrix4x4 R = Matrix4x4.identity;
+        Matrix4x4 S = Matrix4x4.identity;
+        Matrix4x4 TD = Matrix4x4.identity;
+
+        
+        
+
         mCombinedParentXform = parentXform * orgT * trs;
 
-        mCombinedParentXformFromOrigin = parentXFormFromOrigin * trsOrigin;
+        
+        T[12] =  mCombinedParentXform[12];
+        T[13] = mCombinedParentXform[13];
+        T[14] = mCombinedParentXform[14];
+        R = Matrix4x4.Rotate(transform.localRotation * Quaternion.Inverse(oRotation));
+        S[0] = (transform.localScale - oScale + Vector3.one).x;
+        S[5] = (transform.localScale - oScale + Vector3.one).y;
+        S[10] = (transform.localScale - oScale + Vector3.one).z;
+        TD[12] =  (transform.localPosition - oPosition).x;
+        TD[13] = (transform.localPosition - oPosition).y;
+        TD[14] = (transform.localPosition - oPosition).z;
+
+        mCombinedParentXformFromOrigin = parentXFormFromOrigin * TD * T * R * S * T.inverse;
 
         // propagate to all children
         foreach (Transform child in transform)
