57a3f491c173dbbfe0065cc920b681e447c44362
thesquashedman <76268971+thesquashedman@users.noreply.github.com>
2022-12-08 22:08:17 -0800

    Added some semi working inverse kinematics

--- Assets/VRControllerTrack.cs
+++ Assets/VRControllerTrack.cs
@@ -7,10 +7,14 @@ public class VRControllerTrack : MonoBehaviour
     // Start is called before the first frame update
     public Transform handBone;
     public Transform armBone;
+
+    public Transform elbowBone;
     public Transform headset;
 
 
     public int direction;
+
+    float maxReach = 6;
 
 
     bool ready = false;
@@ -28,10 +32,16 @@ public class VRControllerTrack : MonoBehaviour
         {
             
             Quaternion x = Quaternion.FromToRotation(-headset.parent.right * direction, (headset.position - this.transform.position).normalized) * Quaternion.AngleAxis(direction * 80, Vector3.forward);
+
             armBone.localRotation = x;
-            
+            float distance = Mathf.Min(maxReach, (headset.position - this.transform.position).magnitude);
+            float degrees = (maxReach - distance) * 90/maxReach;
+            x *= Quaternion.AngleAxis(degrees, x * -Vector3.up * direction);
+            elbowBone.localRotation = Quaternion.AngleAxis(degrees, -Vector3.right) ;
+            armBone.localRotation = x;
             //Debug.Log(Mathf.Acos(Vector3.Dot(headset.right * -direction, (headset.localPosition - this.transform.localPosition).normalized)) * 180 / Mathf.PI);
             //armBone.localRotation = 
+            
             handBone.localRotation = Quaternion.Inverse(x) * this.transform.localRotation * Quaternion.AngleAxis(-90, Vector3.right) ;
         }
     }
@@ -39,5 +49,6 @@ public class VRControllerTrack : MonoBehaviour
     {
         yield return new WaitForSeconds(1.0f);
         ready = true;
+        maxReach = (headset.position - this.transform.position).magnitude;
     }
 }
